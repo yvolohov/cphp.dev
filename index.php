7aa5d9f446e0e1c5b943af02b2a1c73162edd59e
Yaroslav Volohov <yvolohov@gmail.com>
2016-09-18 21:31:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Lessons\Basics as Basics;
+
+echo "<pre>";
+
+//Basics\punctuation();
+//Basics\tags();
+//Basics\comments();
+//Basics\arithmeticOperators();
+Basics\stringOperators();
+
+echo "</pre>";
